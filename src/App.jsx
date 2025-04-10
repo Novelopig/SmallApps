84db040894b5dd86275ef33fc899d853422eb0d5
Novelopig <161075555+Novelopig@users.noreply.github.com>
2025-04-09 22:37:11 -0400

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,59 @@
-import { useState } from 'react'
-import './App.css'
-import Banner from './components/Banner'
-import Footer from './components/Footer'
-import CounterLike from './components/CounterLike'
-import ImageSlider from './components/ImageSlider'
-import TemConverter from './components/TemConverter'
-import TodoList from './components/TodoList'
-import CounterVote from './components/CounterVote'
-import Calculator from './components/Calculator'
-import CountdownTimer from './components/CountdownTimer'
+import { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+import CounterLike from './components/CounterLike';
+import ImageSlider from './components/ImageSlider';
+import TemConverter from './components/TemConverter';
+import TodoList from './components/TodoList';
+import CounterVote from './components/CounterVote';
+import Calculator from './components/Calculator';
+import CountdownTimer from './components/CountdownTimer';
 
 function App() {
-
   return (
-    <>
-
-    <Banner />
+    <Router basename="/SmallApps">
+      <Banner />
     
-    <main>
-
-    <section className="Apps bg-dark-subtle py-4">
-      <div className="container">
-        
-          <div className="row text-center">
-            <div className="col">
-              <CounterLike />
+      <main>
+        <section className="Apps bg-dark-subtle py-4">
+          <div className="container">
+            <div className="row text-center">
+              <div className="col">
+                <CounterLike />
+              </div>
+              <div className="col">
+                <CountdownTimer />
+              </div>
             </div>
-            <div className="col">
-              <CountdownTimer />
+
+            <ImageSlider />
+
+            <div className="row text-center">
+              <div className="col">
+                <TodoList />
+              </div>
+            </div>
+
+            <div className="row text-center">
+              <div className="col">
+                <TemConverter />
+              </div>
+              <div className="col">
+                <CounterVote />
+              </div>
+              <div className="col">
+                <Calculator />
+              </div>
             </div>
           </div>
+        </section>
+      </main>
 
-          <ImageSlider />
-
-          <div className="row text-center">
-            <div className="col">
-              <TodoList />
-            </div>
-          </div>
-
-          <div className="row text-center">
-            <div className="col">
-              <TemConverter />
-            </div>
-            <div className="col">
-              <CounterVote />
-            </div>
-            <div className="col">
-              <Calculator />
-            </div>
-          </div>
-
-      </div>
-    </section>
-
-    </main>
-
-    <Footer />
-    </>
-  )
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
